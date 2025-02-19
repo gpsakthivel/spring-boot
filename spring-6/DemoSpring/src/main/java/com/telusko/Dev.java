f5a158962338554edbcb6f1cad18098d0e2caa7a
gpsakthivel <psakthi24vel@gmail.com>
2025-02-20 02:05:24 +0530

    Added a sample spring project without boot

---
+++ spring-6/DemoSpring/src/main/java/com/telusko/Dev.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+public class Dev {
+
+    public Dev() {
+        System.out.println("Dev constructor");
+    }
+
+    public void build() {
+        System.out.println("Working on Awesome Project");
+    }
+
+}
---
+++ spring-6/DemoSpring/src/main/java/com/telusko/Laptop.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+public class Laptop {
+
+    public Laptop() {
+        System.out.println("Laptop constructor");
+    }
+
+    public void compile() {
+        System.out.println("Compiling...");
+    }
+
+}
